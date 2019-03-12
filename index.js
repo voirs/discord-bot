243fab659a1ce94a08f3aa6866e69e9ea54194ed
Dani <danielagiorgio2@gmail.com>
2019-03-11 22:31:03 -0300

    add cmmnts

--- index.js
+++ index.js
@@ -6,28 +6,28 @@ var GphApiClient = require('giphy-js-sdk-core')
 giphy = GphApiClient(giphyToken)
 
 client.once('ready', () => {
-    console.log('Ready!')
+    console.log('Ready!') // displays this message on the console if the bot is up and running
 })
 
 client.on('message', message => {   
-    if(message.member.hasPermission(['KICK_MEMBERS', 'BAN_MEMBERS'])){
+    if(message.member.hasPermission(['KICK_MEMBERS', 'BAN_MEMBERS'])){ //check if user has real discord permissions
     
-    if(message.content.startsWith(`${prefix}kick`)){
+    if(message.content.startsWith(`${prefix}kick`)){ // if the entered command is !kick
     
-    let member = message.mentions.members.first();
-    member.kick().then((member) => {
+    let member = message.mentions.members.first(); // will look for a mentioned member inside the msg 
+    member.kick().then((member) => { // kicks the mentioned member and then does aaall this..
 
-        giphy.search('gifs', {"q": "fail"})
+        giphy.search('gifs', {"q": "fail"}) // look for 'fail' gifs in the giphy api
             .then((response) => {
-                var totalResponses = response.data.length;
-                var responseIndex = Math.floor((Math.random() * 10) +1) % totalResponses;
-                var responseFinal = response.data[responseIndex];
+                var totalResponses = response.data.length; // amount of gifs
+                var responseIndex = Math.floor((Math.random() * 10) +1) % totalResponses; // makes sure the gifs  are randomized 
+                var responseFinal = response.data[responseIndex]; // chosen gif
                 
-                message.channel.send(":wave: " + member.displayName + " has been kicked!", {
-                    files: [responseFinal.images.fixed_height.url]
+                message.channel.send(":wave: " + member.displayName + " has been kicked!", { // bot will send an emoji, the kicked member's name and the rest of the message
+                    files: [responseFinal.images.fixed_height.url] // object which is the gif file
                 })
             }).catch(() => {
-                message.channel.send('Error ugh!');
+                message.channel.send('Error ugh!'); //catch any possible errors
             })          
        })
       }
